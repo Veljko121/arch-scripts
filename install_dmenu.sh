be76966977ff041fde228ec437230e8d02c0209c
Veljko121 <109314562+Veljko121@users.noreply.github.com>
2023-03-01 20:47:42 +0000

    Update install_dmenu.sh
    Added base-devel dependency

--- install_dmenu.sh
+++ install_dmenu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # This script installs dwm for arch
 
-doas pacman -S --needed --noconfirm git libxinerama
+doas pacman -S --needed --noconfirm base-devel git libxinerama
 cd
 git clone https://git.suckless.org/dmenu
 cd dmenu
