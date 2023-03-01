364ad78f2dbf5dbc0d4d6e8ca75da09d37bcd754
Veljko121 <109314562+Veljko121@users.noreply.github.com>
2023-03-01 20:49:26 +0000

    Add files via upload

---
+++ install_st.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# This script installs st terminal emulator for arch
+
+doas pacman -S --needed --noconfirm base-devel git
+cd
+git clone https://git.suckless.org/st
+cd st
+doas make clean install
+echo 'st installed successfully!'
+echo 'NOTE: st source code is located in your home folder.'
